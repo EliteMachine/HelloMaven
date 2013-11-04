30878203fd42cb4677bba9bc828f9c0c511825c6
chronicall <Entari@gmail.com>
2013-11-04 16:01:38 +0000

    Maven test project and build scripts

---
+++ HelloMaven/src/main/java/is/ru/hellomaven/HelloMaven.java
@@ -0,0 +1,8 @@
+package is.ru.hellomaven;
+
+public class HelloMaven {
+    public static void main(String[] args) {
+        World world = new World();
+        System.out.println(world.greet());
+    }
+}
