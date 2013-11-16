ebe90a33b19b14465a79ff9b895c4fa5dc31d79c
chronicall <Entari@gmail.com>
2013-11-16 15:08:09 +0000

    Deleting files. Cleaning up.

--- src/main/java/is/ru/hellomaven/HelloMaven.java
+++
@@ -1,8 +0,0 @@
-package is.ru.hellomaven;
-
-public class HelloMaven {
-    public static void main(String[] args) {
-        World world = new World();
-        System.out.println(world.greet());
-    }
-}
